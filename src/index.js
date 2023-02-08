f78d5b42b25c015efc641b4dda976e1abfe2ca18
Ankit Singh <soulankit1234@gmail.com>
2023-02-08 22:50:29 +0530

    Made DB repositories

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const { urlencoded } = require('body-parser');
+const CityRepository = require("./repository/city-repository");
+//const { City } = require("./models/index");
 
 const SetupAndStartServer = async () => {
     // Creating express object
@@ -10,8 +12,14 @@ const SetupAndStartServer = async () => {
     app.use(bodyParser.json);
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.listen(PORT,() => {
+    app.listen(PORT,async () => {
         console.log(`Server started at ${PORT}`);
+        const repo = new CityRepository();
+        repo.createCity({name: "New Delhi"});
+        //await City.create({
+        //    name: "Hyderabad"
+        //})
+
     });
 }
 SetupAndStartServer();
